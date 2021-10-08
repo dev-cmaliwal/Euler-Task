15cf47e451d972361ccc83d826133c1c4d10579c
Chirag <cmaliwal@deqode.com>
2021-10-08 18:55:10 +0530

    Added intefaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,46 @@
+import { Route, Trade } from "@uniswap/sdk";
+
+export interface GraphData {
+  date: number;
+  dailyVolumeUSD: string;
+}
+
+export interface PricingData {
+  route: Route;
+  trade: Trade;
+}
+
+export interface PastSwap {
+  id: string;
+  pair: {
+    token0: {
+      symbol: string;
+    };
+    token1: {
+      symbol: string;
+    };
+  };
+  sender: string;
+  to: string;
+  amount0In: string;
+  amount0Out: string;
+  amount1In: string;
+  amount1Out: string;
+  amountUSD: string;
+}
+
+export interface PairData {
+  id: string;
+  token0: {
+    name: string;
+    symbol: string;
+  };
+  token1: {
+    name: string;
+    symbol: string;
+  };
+  volumeUSD: string;
+  totalSupply: string;
+  reserveETH: string;
+  reserveUSD: string;
+}
