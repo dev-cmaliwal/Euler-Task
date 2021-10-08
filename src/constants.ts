bfdf878588343d982ebb19d2034d975e55a8cf16
Chirag <cmaliwal@deqode.com>
2021-10-08 18:54:57 +0530

    Added constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const PRECISION_VALUE = 12;
+export const DEFAULT_SWAP_VALUE = 0;
+
+export const wallets = [{ walletName: "metamask" }];
+
+export const networkId = 3;
+export const apiKey = "d44f7633-cf88-4e9d-867b-25d07d06ee12";
